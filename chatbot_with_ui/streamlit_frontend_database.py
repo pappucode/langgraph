5de7ab6a9fa92834e4caff109665becf723dc9f8
pappucode <pappuakondo@gmail.com>
2025-08-23 19:32:42 +0600

    add langsmith support to our chatbot

--- chatbot_with_ui/streamlit_frontend_database.py
+++ chatbot_with_ui/streamlit_frontend_database.py
@@ -111,7 +111,14 @@ if user_input:
         st.text(user_input)
 
     # st.session_state -> dict -> 
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    CONFIG = {
+        'configurable': {'thread_id': st.session_state['thread_id']},
+        'metadata': {
+            'thread_id': st.session_state['thread_id']
+        },
+        'run_name': 'chat_turn'
+        
+        }
 
     with st.chat_message('assistant'):
 
